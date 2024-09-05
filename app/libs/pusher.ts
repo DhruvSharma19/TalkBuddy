8a94162719b069103e85d450d17386a24da5f997
Dhruv Sharma <dhruvamsh@gmail.com>
2024-09-05 11:45:58 +0530

    Changes in Json

--- app/libs/pusher.ts
+++ app/libs/pusher.ts
@@ -5,15 +5,7 @@ export const pusherServer = new PusherServer({
   appId: process.env.PUSHER_APP_ID!,
   key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY!,
   secret: process.env.PUSHER_SECRET!,
-<<<<<<< Updated upstream
   cluster: 'ap2',
-=======
-<<<<<<< HEAD
-  cluster: "ap2",
-=======
-  cluster: 'ap2',
->>>>>>> cb3fc3a50257e5aa2b5bb41043d7d59e361ad6d0
->>>>>>> Stashed changes
   useTLS: true,
 });
 
@@ -24,14 +16,6 @@ export const pusherClient = new PusherClient(
       endpoint: "/api/pusher/auth",
       transport: "ajax",
     },
-<<<<<<< Updated upstream
-    cluster: 'ap2',
-=======
-<<<<<<< HEAD
-    cluster: "ap2",
-=======
-    cluster: 'ap2',
->>>>>>> cb3fc3a50257e5aa2b5bb41043d7d59e361ad6d0
->>>>>>> Stashed changes
+    cluster: 'ap2'
   }
 );
